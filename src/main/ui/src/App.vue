44cc2be2b1c42e7aa10bcf3f37af712382a1f5e8
Jonah Morgan <jonah3254@gmail.com>
2021-11-08 03:12:30 -0800

    Configured frontend with router and Vuex store

--- src/main/ui/src/App.vue
+++ src/main/ui/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"> 
+    <router-view />
+    <Home /> 
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Home from './views/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home, 
   }
 }
 </script>
