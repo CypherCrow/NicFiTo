44531c33b1928dc2b1d9c2f8b1ccbe8608e40c3f
Jonah Morgan <jonah3254@gmail.com>
2021-11-07 17:26:39 -0800

    Created new project for the Theta Hackathon

---
+++ src/main/java/project/hackathon/theta/jonah/nicfito/NicFiToApplication.java
@@ -0,0 +1,13 @@
+package project.hackathon.theta.jonah.nicfito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicFiToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicFiToApplication.class, args);
+	}
+
+}
