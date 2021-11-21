db0b10b00982fe79a3088312bd5583de34a0f536
Jonah Morgan <jonah3254@gmail.com>
2021-11-21 00:42:25 -0800

    Got the full stack working. HOORAY!

---
+++ src/main/java/project/hackathon/theta/jonah/nicfito/controllers/MessageController.java
@@ -0,0 +1,16 @@
+package project.hackathon.theta.jonah.nicfito.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+    
+    @GetMapping("/hello")
+    public String hello(){
+        return "Where fictional works and authors shine. An NFT bookstore, basically";
+    }
+
+}
