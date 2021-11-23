55b2ede6516293fac54da217cf777ac06208a06c
Jonah Morgan <jonah3254@gmail.com>
2021-11-23 03:07:01 -0800

    Added About.vue

---
+++ src/ui/src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+    <div> 
+        <Header /> 
+
+        <article>
+            <section> 
+                <h1>What is NicFiTo?</h1>
+
+                <p>NicFiTo is an NFT marketplace for fictional works, deployed on the Theta blockchain. These fictional works range from short stories to movies.</p>
+            </section>
+        </article>
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+
+export default {
+    name: 'About', 
+    components: {
+        Header, 
+    }
+}
+</script>
