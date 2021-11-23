b0e40a9429f09cdeb7456186bb64270b6f1cee6b
Jonah Morgan <jonah3254@gmail.com>
2021-11-23 03:01:06 -0800

    Added ui's /assets content

---
+++ src/ui/src/views/About.vue
@@ -0,0 +1,33 @@
+<template>
+    <div> 
+        <Header />
+
+        <article> 
+            <section> 
+                <h1>What is NicFiTo?</h1> 
+
+                <p>NicFiTo is an NFT marketplace where authors, animators and filmmakers sell their works without risk of plagiarism.</p>
+            </section>
+        </article>
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+
+export default {
+    name: 'About', 
+    components: {
+        Header, 
+    }
+}
+</script>
+
+<style scoped>
+
+section h1 {
+    font-size: 2em; 
+    margin-bottom: 3.2rem; 
+}
+
+</style>
