4b3a6e1e05578ecc490864686f69b390443d69cc
Jonah "Jon" Morgan <57231486+CypherCrow@users.noreply.github.com>
2021-11-26 01:11:45 -0800

    Added more contracts for Book, Comics, and Series

---
+++ src/main/resources/contracts/NicfitoComics.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.10; 
+
+import "./CoolNFT.sol"; 
+
+contract NicfitoComics is CoolNFT {
+    constructor() CoolNFT("Comics", "COMICS" ) public {
+
+    }
+}
---
+++ src/main/resources/contracts/NicfitoSeries.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.10; 
+
+import "./CoolNFT.sol"; 
+
+contract NicfitoSeries is CoolNFT {
+    constructor() CoolNFT("Series", "SERIES") public {
+        
+    }
+}
