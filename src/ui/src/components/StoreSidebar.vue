95f2e3069fc19df725888fa9694d0b0bd0795803
Jonah "Jon" Morgan <57231486+CypherCrow@users.noreply.github.com>
2021-11-27 02:28:57 -0800

    Added JavaScript Elasticsearch library, created new component StoreSidebar that's customizable, started implementation of MongoDB with Spring.

---
+++ src/ui/src/components/StoreSidebar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="sidebar">
+        <h3 id="name">Store Assistant</h3>
+    </div>
+</template>
+
+<script> 
+
+export default {
+    name: 'StoreSidebar'
+}
+
+</script> 
+
+<style scoped> 
+div#sidebar {
+    float: right; 
+    background-color: chartreuse;
+    justify-content: center;
+    align-items: center;
+    padding: 10px; 
+    margin-right: 30px;
+    width: 200px;
+    height: 500px;
+    border-radius: 1.3rem; 
+}
+</style>
